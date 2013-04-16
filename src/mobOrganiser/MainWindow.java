42587cd2af62928ccca49c681db7cc722fd2d469
Natalia Amezquita <nataliaamezquita@138-38-140-15.eduroam.bath.ac.uk>
2013-04-16 12:02:37 +0100

    MainWindow and MenuBarPanel
    Added MenuBarPanel to the MainWindow

--- src/mobOrganiser/MainWindow.java
+++ src/mobOrganiser/MainWindow.java
@@ -1,7 +1,40 @@
 package mobOrganiser;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class MainWindow extends JFrame {
-
+public class MainWindow extends JFrame 
+{
+	
+	public MainWindow()
+	{	
+		initialiseComponents();
+	}
+	
+	public void initialiseComponents()
+	{
+		MenuBarPanel menuBar = new MenuBarPanel();
+		JPanel mainPanel = new JPanel();
+		mainPanel.setPreferredSize(new Dimension(200,280));
+		
+		mainPanel.add(menuBar, BorderLayout.NORTH);
+		add(mainPanel);
+	}
+	
+	public void setMainFrameVisible(MainWindow window)
+	{
+		window.setTitle("Agenda");
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		MainWindow window = new MainWindow();
+		window.setMainFrameVisible(window);
+	}
 }
--- src/mobOrganiser/MenuBarPanel.java
+++ src/mobOrganiser/MenuBarPanel.java
@@ -1,7 +1,33 @@
 package mobOrganiser;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MenuBarPanel extends JPanel {
-
+	
+	private JButton calendarButton;
+	private JButton todayButton;
+	private JButton contactsButton;
+	
+	public MenuBarPanel()
+	{
+		calendarButton = new JButton("Calendar");
+		todayButton = new JButton("Today");
+		contactsButton = new JButton("Contacts");
+		intialiseComponents();
+	}
+	
+	public void intialiseComponents()
+	{	
+		setLayout(new GridLayout(1,3));
+		
+		setPreferredSize(new Dimension(200,35));
+		
+		add(calendarButton);
+		add(todayButton);
+		add(contactsButton);
+	}
 }
