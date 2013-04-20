50327f20333c91ba2362a0657402a362ce9e1938
Steven Borrie <sab66@bath.ac.uk>
2013-04-20 17:51:14 +0100

    Changed the layout manager in menu bar
    changed from grid view to spring layout, also changed the string to be
    the first character of the button because im going to replace the button
    labels with image icons in the next commit

--- src/mobOrganiser/MenuBarPanel.java
+++ src/mobOrganiser/MenuBarPanel.java
@@ -5,6 +5,7 @@ import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 public class MenuBarPanel extends JPanel {
 	
@@ -14,20 +15,39 @@ public class MenuBarPanel extends JPanel {
 	
 	public MenuBarPanel()
 	{
-		calendarButton = new JButton("Calendar");
-		todayButton = new JButton("Today");
-		contactsButton = new JButton("Contacts");
+		calendarButton = new JButton("C");
+		todayButton = new JButton("T");
+		contactsButton = new JButton("C");
 		intialiseComponents();
 	}
 	
 	public void intialiseComponents()
 	{	
-		setLayout(new GridLayout(1,3));
+		SpringLayout layout = new SpringLayout();
+		//setLayout(new GridLayout(1,3));
+		setLayout(layout);
 		
 		setPreferredSize(new Dimension(200,35));
 		
 		add(calendarButton);
 		add(todayButton);
 		add(contactsButton);
+		
+		Dimension dimension1 = new Dimension(66, 35);
+		Dimension dimension2 = new Dimension(68, 35);
+		
+		calendarButton.setPreferredSize(dimension1);
+		todayButton.setPreferredSize(dimension2);
+		contactsButton.setPreferredSize(dimension1);
+		
+		
+		//align all 3 buttons with the top of this panel.
+		layout.putConstraint(SpringLayout.NORTH, calendarButton, 0, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, todayButton, 0, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.NORTH, contactsButton, 0, SpringLayout.NORTH, this);
+		
+		layout.putConstraint(SpringLayout.WEST, calendarButton, 0, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.WEST, todayButton, 0, SpringLayout.EAST, calendarButton);
+		layout.putConstraint(SpringLayout.EAST, contactsButton, 0, SpringLayout.EAST, this);
 	}
 }
